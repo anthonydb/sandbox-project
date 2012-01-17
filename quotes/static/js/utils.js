27e07cf02f34b30c798af96a51899c7476f131e4
adebarros <adebarros@gmail.com>
2012-01-16 22:21:44 -0500

    Refining form validation

--- quotes/static/js/utils.js
+++ quotes/static/js/utils.js
@@ -1,9 +1,18 @@
 function validtest() {
-   var boxValue = document.forms["suggestForm"]["validateBox"].value.toLowerCase();    
-   if (boxValue != "blue")
+   var quoteboxValue = document.forms["suggestForm"]["id_item"].value.toLowerCase();
+   var validboxValue = document.forms["suggestForm"]["validateBox"].value.toLowerCase();    
+   
+   if (quoteboxValue == "")
+      {
+         var strMessage = "You must enter a quote.";
+         document.getElementById('message').innerHTML = strMessage;
+         return false;
+      }
+   else if (validboxValue != "blue")
       {
          var strMessage = "Validation is incorrect.";
          document.getElementById('message').innerHTML = strMessage;
          return false;
       }
+   return true;
 }
