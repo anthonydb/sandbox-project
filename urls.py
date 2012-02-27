c8241c4c327ad9e17fe24fe1685a41f26240302e
adebarros <adebarros@gmail.com>
2012-02-26 22:43:39 -0500

    Minor tweaks.

--- urls.py
+++ urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^words/', include('quotes.urls')),
     url(r'^apis/', include('apis.urls')),
+#ll
     (r'^$', include('core.urls.home')),
 )
