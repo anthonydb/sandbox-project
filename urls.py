9e9f0d56c2489da7f4db5dc720eaee794cbeaa7f
adebarros <adebarros@gmail.com>
2012-02-27 21:07:20 -0500

    Security-related.

--- urls.py
+++ urls.py
@@ -1,12 +1,1 @@
-from django.conf.urls.defaults import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^words/', include('quotes.urls')),
-    url(r'^apis/', include('apis.urls')),
-    #cc
-    (r'^$', include('core.urls.home')),
-)
+from urls_base import *
