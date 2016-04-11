a021b5031789314aa1bfdac451e46070fec699de
anthonydb <adebarros@gmail.com>
2016-04-10 22:49:48 -0400

    Restyling and responsiveness for API example.

--- apis/views.py
+++ apis/views.py
@@ -19,22 +19,30 @@ def BookListAPIView(request):
     today = date.today()
     offset = (today.weekday() - 3) % 7
     thursday = today - timedelta(days=(offset + 7))
-    date_dict = {'year': str(thursday.year), 'month': str(thursday.month), 'day': str(thursday.day)}
+    date_dict = {'year': str(thursday.year),
+                 'month': str(thursday.month),
+                 'day': str(thursday.day)}
 
-    # prep and load URL
+    # Prep and load URL.
     base_url = 'http://usatoday30.usatoday.com/api/books/'
     url = base_url + date_dict['year'] + '/' + date_dict['month'] + '/' + date_dict['day']
     r = requests.get(url)
 
     top_list = []
     try:
-        # If JSON was returned, parse it
+        # If JSON was returned, parse it.
         booklist = json.loads(r.text)
         for x in range(0, 20):
             books_dict = {}
             books_dict['rank'] = booklist['BookLists'][0]['BookListEntries'][x]['Rank']
             books_dict['title'] = booklist['BookLists'][0]['BookListEntries'][x]['Title']
             books_dict['author'] = booklist['BookLists'][0]['BookListEntries'][x]['Author']
+            if booklist['BookLists'][0]['BookListEntries'][x]['Class'] == 'Fiction':
+                books_dict['class'] = '(F)'
+            elif booklist['BookLists'][0]['BookListEntries'][x]['Class'] == 'NonFiction':
+                books_dict['class'] = '(NF)'
+            else:
+                books_dict['class'] = '(X)'
             top_list.append(books_dict)
     except ValueError:
         # Pass along a message that the API did not respond
