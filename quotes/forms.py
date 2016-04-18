9e5e3cacd65e7bbbb6a852d98198b036fc220548
anthonydb <adebarros@gmail.com>
2016-04-17 21:18:43 -0400

    Styling for quote suggestion form.

--- quotes/forms.py
+++ quotes/forms.py
@@ -5,16 +5,17 @@ from django.http import HttpResponseRedirect
 from django import forms
 from django.forms import ModelForm, Textarea
 
-# Suggest-a-quote form
-
 
 class SuggestForm(ModelForm):
+    """
+    Form for suggesting a quote.
+    """
     item = forms.CharField(label='Quote',
                            widget=Textarea(attrs={'cols': 60, 'rows': 10})
                            )
     source = forms.CharField(label='Source',
-                           widget=Textarea(attrs={'cols': 60, 'rows': 5})
-                           )
+                             widget=Textarea(attrs={'cols': 60, 'rows': 5})
+                             )
 
     class Meta:
         model = Submission
@@ -29,21 +30,21 @@ def submit_quote(request):
             obj.ip_address = request.META.get('REMOTE_ADDR', 'Unknown')
             obj.misc_headers = str(request.META.get('REMOTE_HOST', 'Unknown')) + '|' + str(request.META.get('HTTP_USER_AGENT', 'Unknown'))
             obj.save()
-            #form.save()
             return HttpResponseRedirect('/words/suggest/thanks/')
     else:
         form = SuggestForm()
     return render_to_response('suggest.html',
-                                {'form': form},
-                                context_instance=RequestContext(request))
-
-# Search quotes form
+                              {'form': form},
+                              context_instance=RequestContext(request))
 
 
 class SearchForm(ModelForm):
+    """
+    Form for searching quotes.
+    """
     quote = forms.CharField(label='Term',
                             widget=Textarea(attrs={'cols': 40, 'rows': 1})
-                           )
+                            )
 
     class Meta:
         model = Quote
@@ -59,10 +60,10 @@ def search_quotes(request):
             if not results:
                 results = ['None']
             return render_to_response('search.html',
-                                        {'form': form, 'results_list': results},
-                                        context_instance=RequestContext(request))
+                                      {'form': form, 'results_list': results},
+                                      context_instance=RequestContext(request))
     else:
         form = SearchForm()
     return render_to_response('search.html',
-                             {'form': form},
-                             context_instance=RequestContext(request))
+                              {'form': form},
+                              context_instance=RequestContext(request))
