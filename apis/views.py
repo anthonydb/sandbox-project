b90dfd6fa4cbd062010682d8a75f9c1d3c4c5f60
anthonydb <adebarros@gmail.com>
2016-12-27 22:14:24 -0500

    Switch to NYT book list since USAT API is kaput.

--- apis/views.py
+++ apis/views.py
@@ -3,10 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.core.mail import mail_admins
 import requests
-import json
-# from keys import *
+from .keys import *
 from datetime import date
-from datetime import timedelta
 
 
 class APIHomeView(TemplateView):
@@ -14,36 +12,29 @@ class APIHomeView(TemplateView):
 
 
 def BookListAPIView(request):
-    # Find a Thursday one week prior to today; the public
-    # Book List API offers the list on a one-week delay.
-    today = date.today()
-    offset = (today.weekday() - 3) % 7
-    thursday = today - timedelta(days=(offset + 7))
-    date_dict = {'year': str(thursday.year),
-                 'month': str(thursday.month),
-                 'day': str(thursday.day)}
+    # Construct API url
+    api_key = nyt_api
+    base_url = 'https://api.nytimes.com/svc/books/v3/lists.json'
+    payload = {'api-key': api_key,
+               'list': 'combined-print-and-e-book-nonfiction'}
 
-    # Prep and load URL.
-    base_url = 'http://usatoday30.usatoday.com/api/books/'
-    url = base_url + date_dict['year'] + '/' + date_dict['month'] + '/' + date_dict['day']
-    r = requests.get(url)
-
-    top_list = []
+    # Attempt to fetch the API and build a list of titles
     try:
-        # If JSON was returned, parse it.
-        booklist = json.loads(r.text)
-        for x in range(0, 20):
+        r = requests.get(base_url, params=payload)
+        nyt_list_response = r.json()
+        nyt_list_date = nyt_list_response['results'][0]['bestsellers_date']
+
+        # A list of dicts for titles
+        top_list = []
+
+        for book in nyt_list_response['results']:
             books_dict = {}
-            books_dict['rank'] = booklist['BookLists'][0]['BookListEntries'][x]['Rank']
-            books_dict['title'] = booklist['BookLists'][0]['BookListEntries'][x]['Title']
-            books_dict['author'] = booklist['BookLists'][0]['BookListEntries'][x]['Author']
-            if booklist['BookLists'][0]['BookListEntries'][x]['Class'] == 'Fiction':
-                books_dict['class'] = '(F)'
-            elif booklist['BookLists'][0]['BookListEntries'][x]['Class'] == 'NonFiction':
-                books_dict['class'] = '(NF)'
-            else:
-                books_dict['class'] = '(X)'
+            books_dict['rank'] = str(book['rank'])
+            books_dict['title'] = book['book_details'][0]['title']
+            books_dict['author'] = book['book_details'][0]['author']
+            books_dict['amazon_link'] = book['amazon_product_url']
             top_list.append(books_dict)
+
     except ValueError:
         # Pass along a message that the API did not respond
         books_dict = {}
@@ -52,5 +43,5 @@ def BookListAPIView(request):
         mail_admins('Books API', 'Looks like the Books API failed.')
 
     return render_to_response('booklist.html',
-                              {'top_list': top_list, 'date_dict': date_dict},
+                              {'top_list': top_list, 'list_date': nyt_list_date},
                               context_instance=RequestContext(request))
